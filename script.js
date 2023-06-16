7a4901396b01cf26a3cf623fcdf9a328453e36a0
initial commit <acpreciousgertrude@gmail.com>
2023-06-16 18:52:33 +0100

    commit

---
+++ script.js
@@ -0,0 +1,51 @@
+alert("my form is functional")
+prompt("Do you like my form?", "Yes, It is beautiful"); 
+console.log("Thank you for enjoying the experience.")
+console.error("We will work on this.")
+/*I comment my reserve in multiple lines.*/
+//I am keeping my comments in a single line.
+
+//#Variable
+let understood=true;
+let age=20;
+var pie = 0.22
+var name="Wale Wahala";
+let nullVariable = null;
+var undefinedVariable = undefined;
+let toun
+console.log(toun);//Toun will be undefined
+// ##Constant is used to initialise fixed values that dont change during execution tme.
+console.log(typeof understood);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof nullVariable);
+console.log(typeof undefinedVariable);
+console.log(pie);
+
+// #Operators
+/*
+Arithmetic
+           ** Means raised to power (Exponenciation)
+           + Addition
+           - Subtraction
+           ++ Increment
+           -- Decrement
+           * Multiplication
+Comparison Operators
+           ==, ===, !=, !== <, > >=, <==
+Assignment Operators
+            =, -=, +=, *=, /=
+Logical Operators
+            and &&, or ||, not !
+Tenary Operators            
+*/
+
+let x = 2;
+console.log(++x);
+console.log(2**2**3*3);
+console.log(5==3);
+console.log(3>2>1===false);
+console.log(7>5&&5>1);
+console.log(!(5>7));
+
+
